36b7072630a7576acc879d6f5586283aa9087d71
Ed Kaim <ed@sharplogic.com>
2020-02-24 23:49:34 -0800

    Fixed scores

--- Tailspin.SpaceGame.LeaderboardContainer/Models/Score.cs
+++ Tailspin.SpaceGame.LeaderboardContainer/Models/Score.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace TailSpin.SpaceGame.LeaderboardContainer
 {
@@ -6,18 +7,22 @@ namespace TailSpin.SpaceGame.LeaderboardContainer
     {
         // The ID of the player profile associated with this score.
         [JsonProperty(PropertyName = "profileId")]
+        [JsonPropertyName("profileId")]
         public string ProfileId { get; set; }
 
         // The score value.
         [JsonProperty(PropertyName = "score")]
+        [JsonPropertyName("score")]
         public int HighScore { get; set; }
 
         // The game mode the score is associated with.
         [JsonProperty(PropertyName = "gameMode")]
+        [JsonPropertyName("gameMode")]
         public string GameMode { get; set; }
 
         // The game region (map) the score is associated with.
         [JsonProperty(PropertyName = "gameRegion")]
+        [JsonPropertyName("gameRegion")]
         public string GameRegion { get; set; }
     }
 }
