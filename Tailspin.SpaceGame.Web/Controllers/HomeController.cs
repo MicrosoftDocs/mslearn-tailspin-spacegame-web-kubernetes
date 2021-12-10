1d25b838b62b6a013ff442eda2af86608ab3d4cb
Thomas Petchel <tpetchel@gmail.com>
2021-12-10 13:30:24 -0800

    Simplify LINQ queries

--- Tailspin.SpaceGame.Web/Controllers/HomeController.cs
+++ Tailspin.SpaceGame.Web/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TailSpin.SpaceGame.Web.Models;
